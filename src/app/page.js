e43b4614a3454a68d0ceab0dc37ca39077196340
Luiz Casara <luiz.casara@maxxidata.com>
2023-05-17 22:09:07 -0300

    add images

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400">
-      <div className="w-full bg-white py-8">
+      <div className="w-full bg-white py-20">
         <img
           src="/banner1.jpeg"
           alt="Banner"
@@ -26,7 +26,43 @@ export default function Home() {
             {/* Item de produto */}
             <li className="bg-white rounded shadow p-4">
               <img
-                src="caminho_para_imagem"
+                src="/bolo1.jpg"
+                alt="Produto 1"
+                className="w-full h-40 object-cover mb-4 rounded"
+              />
+              <h3 className="text-lg font-semibold mb-2">Nome do Produto 1</h3>
+              <p className="text-gray-600">Descrição do Produto 1</p>
+            </li>
+            <li className="bg-white rounded shadow p-4">
+              <img
+                src="/bolo1.jpg"
+                alt="Produto 1"
+                className="w-full h-40 object-cover mb-4 rounded"
+              />
+              <h3 className="text-lg font-semibold mb-2">Nome do Produto 1</h3>
+              <p className="text-gray-600">Descrição do Produto 1</p>
+            </li>
+            <li className="bg-white rounded shadow p-4">
+              <img
+                src="/bolo1.jpg"
+                alt="Produto 1"
+                className="w-full h-40 object-cover mb-4 rounded"
+              />
+              <h3 className="text-lg font-semibold mb-2">Nome do Produto 1</h3>
+              <p className="text-gray-600">Descrição do Produto 1</p>
+            </li>
+            <li className="bg-white rounded shadow p-4">
+              <img
+                src="/bolo1.jpg"
+                alt="Produto 1"
+                className="w-full h-40 object-cover mb-4 rounded"
+              />
+              <h3 className="text-lg font-semibold mb-2">Nome do Produto 1</h3>
+              <p className="text-gray-600">Descrição do Produto 1</p>
+            </li>
+            <li className="bg-white rounded shadow p-4">
+              <img
+                src="/bolo1.jpg"
                 alt="Produto 1"
                 className="w-full h-40 object-cover mb-4 rounded"
               />
@@ -40,7 +76,7 @@ export default function Home() {
           <h2 className="text-3xl font-bold mb-4">Redes Sociais</h2>
           <div className="flex space-x-4">
             <a
-              href="link_do_instagram"
+              href="https://www.facebook.com/docesgus"
               className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
             >
               <img
@@ -50,7 +86,7 @@ export default function Home() {
               />
             </a>
             <a
-              href="link_do_facebook"
+              href="https://www.instagram.com/gusloseimas/"
               className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
             >
               <img
@@ -60,7 +96,7 @@ export default function Home() {
               />
             </a>
             <a
-              href="link_do_whatsapp"
+              href="https://www.tiktok.com/@gusloseimas?lang=pt-BR"
               className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
             >
               <img
@@ -70,7 +106,7 @@ export default function Home() {
               />
             </a>
             <a
-              href="link_do_whatsapp"
+              href="https://api.whatsapp.com/send?phone=5545988214352&fbclid=PAAabaHtAwzbeeED42Ho_xvyZXH_5bEOVxvUtHlFGOms50S0QGviZc_31YDTE"
               className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
             >
               <img
@@ -83,112 +119,5 @@ export default function Home() {
         </section>
       </div>
     </div>
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-    //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    //       Get started by editing&nbsp;
-    //       <code className="font-mono font-bold">src/app/page.js</code>
-    //     </p>
-    //     <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-    //       <a
-    //         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{' '}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className="dark:invert"
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-    //     <Image
-    //       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Docs{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Find in-depth information about Next.js features and API.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Learn{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Learn about Next.js in an interactive course with&nbsp;quizzes!
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Templates{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Explore the Next.js 13 playground.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Deploy{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
   )
 }
