582e3486dd03440d6691a30b8091ec1aab3d325b
Luiz Casara <luiz.casara@maxxidata.com>
2023-06-06 09:43:40 -0300

    creating a new component to social midia

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import ImageCarousel from "../components/ImageCarousel";
 import Image from "next/image";
 
+import logo from "../../public/logoBigger.png";
 import bolo1 from "../../public/products/bolo1.png";
-import bolo2 from "../../public/products/bolo2.jpg";
+import bolo2 from "../../public/products/bolo2.png";
 import bolo3 from "../../public/products/bolo3.png";
 import boloVulcao1 from "../../public/products/boloVulcao1.png";
+import boloVulcao2 from "../../public/products/boloVulcao2.png";
+import brownie1 from "../../public/products/brownie1.png";
 import brownie2 from "../../public/products/brownie2.png";
 import docinhos1 from "../../public/products/docinhos1.png";
+import trufas from "../../public/products/trufas.png";
+import noImage from "../../public/products/noImage.png";
 
 export default function Home() {
   function productSection(imgSrx, imgAlt, itemsList) {
@@ -16,6 +21,8 @@ export default function Home() {
           <Image
             src={imgSrx}
             alt={imgAlt}
+            width={320}
+            height={112}
             className="flex w-80 object-cover mb-4 rounded"
           />
         </div>
@@ -46,11 +53,27 @@ export default function Home() {
     ))
   }
 
+  function renderSocial(href, src, text) {
+    return (
+      <a
+        href={href}
+        className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
+      >
+        <Image
+          src={src}
+          alt={text}
+          width={100}
+          height={50}
+        />
+      </a>
+    )
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-serif">
       <div className="w-full bg-rose-100 py-16 justify-center flex">
         <Image
-          src="/logoBigger.png"
+          src={logo}
           alt="Banner"
           className="w-auto h-80 flex"
         />
@@ -83,21 +106,21 @@ export default function Home() {
             },
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: bolo2,
                 name: "Bolo Kinder Bueno",
               }],
               description: "Massa fofinha de chocolate 50% e recheio, Creme Kinder Bueno da casa e brigadeiro de ninho, coberto por ganache de chocolate branco e decorado com kinder bueno.",
             },
             {
               imgs: [{
-                imgSrc: bolo3,
-                name: "Quatro Leites com }]Morango",
+                imgSrc: noImage,
+                name: "Quatro Leites com Morango",
               }],
               description: "Massa branca, úmida, sabor baunilha,  recheada com morangos picados e creme 4 leites bem cremoso.",
             },
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: noImage,
                 name: "Dois Amores",
               }],
               description: "Massa fofinha recheada com os tradicionais brigadeiro branco e brigadeiro preto. Ambos feitos com chocolate nobre. Coberta com ganache branca e preta.",
@@ -108,21 +131,21 @@ export default function Home() {
           {productSection("/products/separators/bolos_volcao.png", "bolos volcao", [
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: boloVulcao1,
                 name: "Red Velvett",
               }],
               description: "Massa vermelhinha e fofinha coberta por creme de cream cheese.",
             },
             {
               imgs: [{
-                imgSrc: boloVulcao1,
+                imgSrc: boloVulcao2,
                 name: "Ninho com Nutella"
               }],
               description: "Massa de chocolate 50% cacau com vulcão cheio de Nutella e cobertura de brigadeiro de leite ninho.",
             },
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: noImage,
                 name: "Chocolate",
               }],
               description: "Massa de cacau 100% coberto por brigadeiro de chocolate nobre ao leite e raspas de chocolate.",
@@ -133,7 +156,7 @@ export default function Home() {
           {productSection("/products/separators/brownie.png", "Brownie", [
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: brownie1,
                 name: "Red Velvett",
               }],
               description: "Massa com chocolate branco nobre e creme de cream cheese.",
@@ -151,21 +174,21 @@ export default function Home() {
           {productSection("/products/separators/sobremesas.png", "Sobremesas", [
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: noImage,
                 name: "Pavê",
               }],
               description: "creme 4 leites, bolacha champagne e cobertura de ganache de chocolate blend.",
             },
             {
               imgs: [{
-                imgSrc: bolo3,
-                name: "Bombom aberto de U}]va ou morango",
+                imgSrc: noImage,
+                name: "Bombom aberto de Uva ou morango",
               }],
               description: "Creme branco coberto por uvas verdes ou morangos (conforme preferir) e ganache de chocolate blend.",
             },
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: noImage,
                 name: "Meio a meio",
               }],
               description: "Creme de leite ninho com ganache de chocolate ao leite.",
@@ -176,7 +199,7 @@ export default function Home() {
           {productSection("/products/separators/trufas.png", "Trufas", [
             {
               imgs: [{
-                imgSrc: bolo3,
+                imgSrc: trufas,
                 name: "Trufas de vários sabores",
               }],
               description: "Chocolate, Chocolate branco, Morango, Maracujá ,Napolitano, Meio amargo.",
@@ -188,65 +211,20 @@ export default function Home() {
             {
               imgs: [{
                 imgSrc: docinhos1,
-                name: "Trufas de vários sabores",
+                name: "Docinhos de vários sabores",
               }],
               description: "Brigadeiro, Maracujá, Morango, Café, Sensação, Ninho, Ninho c/ nutella, Meio amargo.",
             },
           ])}
-
-          {/* Informações extra
-          {productSection("/products/separators/info.png", "Informações", [
-            {
-              imgSrc: null,
-              name: "Trufas de vários sabores",
-              description: "Brigadeiro, Maracujá, Morango, Café, Sensação, Ninho, Ninho c/ nutella, Meio amargo.",
-            },
-          ])} */}
         </section>
 
         <section className="mb-8 bg:white">
           <h2 className="text-3xl font-bold mb-4 text-gray-700">Redes Sociais</h2>
           <div className="flex space-x-4">
-            <a
-              href="https://www.facebook.com/docesgus"
-              className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
-            >
-              <Image
-                src="/facebook.png"
-                className="w-16 h-16 fill-current"
-                alt="face"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/gusloseimas/"
-              className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
-            >
-              <Image
-                src="/instagram.png"
-                className="w-16 h-16 fill-current"
-                alt="instagram"
-              />
-            </a>
-            <a
-              href="https://www.tiktok.com/@gusloseimas?lang=pt-BR"
-              className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
-            >
-              <Image
-                src="/tiktok.png"
-                className="w-16 h-16 fill-current"
-                alt="tiktok"
-              />
-            </a>
-            <a
-              href="https://api.whatsapp.com/send?phone=5545988214352&fbclid=PAAabaHtAwzbeeED42Ho_xvyZXH_5bEOVxvUtHlFGOms50S0QGviZc_31YDTE"
-              className="text-purple-500 hover:text-purple-700 transform hover:translate-y-1 transition-all duration-200"
-            >
-              <Image
-                src="/whatsapp.png"
-                className="w-16 h-16 fill-current"
-                alt="whatsapp"
-              />
-            </a>
+            {renderSocial('https://www.facebook.com/docesgus', '/facebook.png', 'Facebeook')}
+            {renderSocial('https://www.instagram.com/gusloseimas', '/instagram.png', 'Instagram')}
+            {renderSocial('https://www.tiktok.com/@gusloseimas?lang=pt-BR', '/whatsapp.png', 'Whatsapp')}
+            {renderSocial('https://api.whatsapp.com/send?phone=5545988214352&fbclid=PAAabaHtAwzbeeED42Ho_xvyZXH_5bEOVxvUtHlFGOms50S0QGviZc_31YDTE', '/tiktok.png', 'Tiktok')}
           </div>
         </section>
       </div>
